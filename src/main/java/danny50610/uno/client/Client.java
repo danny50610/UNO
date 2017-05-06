bfb2c315fdb58c2a0b42fca80de7b98cd35857c9
Chan, Danny <danny50610@yahoo.com.tw>
2017-05-06 15:33:32 +0800

    Better code

--- src/main/java/danny50610/uno/client/Client.java
+++ src/main/java/danny50610/uno/client/Client.java
@@ -8,6 +8,15 @@ import javax.swing.*;
 public class Client {
 
     public static void main(String[] args) {
+        Client client = new Client();
+        client.run();
+    }
+
+    public Client() {
+        this.run();
+    }
+
+    public void run() {
         JFrame frame = new JFrame("UNO");
         frame.setContentPane(new FormLogin().getPanel1());
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);

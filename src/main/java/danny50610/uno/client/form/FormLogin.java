526fcd9290964ef0c4455f6ad51226f45491afd1
Chan, Danny <danny50610@yahoo.com.tw>
2017-05-06 15:20:56 +0800

    Name Control

--- src/main/java/danny50610/uno/client/form/FormLogin.java
+++ src/main/java/danny50610/uno/client/form/FormLogin.java
@@ -7,8 +7,10 @@ import javax.swing.*;
  */
 public class FormLogin {
     private JPanel panel1;
-    private JTextField textField1;
-    private JPasswordField passwordField1;
-    private JButton button1;
-    private JButton button3;
+    private JTextField usernameTextField;
+    private JPasswordField passwordField;
+    private JButton registerButton;
+    private JButton loginButton;
+    private JLabel passwordLabel;
+    private JLabel usernameLabel;
 }

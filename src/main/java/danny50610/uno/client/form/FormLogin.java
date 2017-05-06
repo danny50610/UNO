2e6f17715283a52ddc28c12f4854ad5ec9f8b6ed
Chan, Danny <danny50610@yahoo.com.tw>
2017-05-06 15:15:43 +0800

    Add Login UI ((╯°O°)╯┻━┻)

---
+++ src/main/java/danny50610/uno/client/form/FormLogin.java
@@ -0,0 +1,14 @@
+package danny50610.uno.client.form;
+
+import javax.swing.*;
+
+/**
+ * Created by danny50610 on 2017/5/6.
+ */
+public class FormLogin {
+    private JPanel panel1;
+    private JTextField textField1;
+    private JPasswordField passwordField1;
+    private JButton button1;
+    private JButton button3;
+}

405add6e6e61c4c53655162f63caa17239d745fe
Chan, Danny <danny50610@yahoo.com.tw>
2017-05-15 23:29:52 +0800

    Add client close Add register form

--- src/main/java/club/dannyserver/uno/client/Client.java
+++ src/main/java/club/dannyserver/uno/client/Client.java
@@ -18,21 +18,52 @@ public class Client {
         } catch (Exception e) {
             e.printStackTrace();
         }
+        finally {
+            if (client != null) {
+                client.close();
+            }
+        }
     }
 
     private final Socket socket;
 
+    private JFrame loginFrame;
+
+    private JFrame registerFrame;
+
     public Client(String ip, int port) throws Exception {
         this.socket = new Socket(ip, port);
+
+        initLoginFrame();
+        initRegisterFrame();
+    }
+
+    private void initLoginFrame() {
+        loginFrame = new JFrame("UNO Login");
+        loginFrame.setContentPane(new FormLogin().getMainPanel());
+        loginFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        loginFrame.setResizable(false);
+        loginFrame.pack();
+    }
+
+    private void initRegisterFrame() {
+        registerFrame = new JFrame("UNO Register");
+        registerFrame.setContentPane(new FormRegister().getMainPanel());
+        registerFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        registerFrame.setResizable(false);
+        registerFrame.pack();
     }
 
     public void run() {
-        JFrame frame = new JFrame("UNO Login");
-        frame.setContentPane(new FormLogin().getMainPanel());
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setResizable(false);
-        frame.pack();
-        frame.setVisible(true);
+        loginFrame.setVisible(true);
+    }
+
+    public void close() {
+        try {
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 }
